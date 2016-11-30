14ff65779f2bb333a99f5e75e8582687b27d07cc
Ronald Jerez <RonaldJerez@users.noreply.github.com>
2016-11-30 16:09:22 -0500

    improvements and modifications

--- bash_profile.sh
+++ bash_profile.sh
@@ -1,76 +1,21 @@
 #!/bin/bash
-# bash profile created by Ronald Jerez (e052007)
+# bash profile created by Ronald Jerez (github.com/ronaldjerez)
 
-style() {
-	local black=30
-	local red=31
-	local green=32
-	local yellow=33
-	local blue=34
-	local purple=35
-	local cyan=36
-	local white=37
-	local dark_grey=90
-	local light_red=91
-	local light_green=92
-	local light_yellow=93
-	local light_blue=94
-	local light_purple=95
-	local turq=96
-	local none=0
-	
-	# the choosen one
-	local color="$1"
+# Regular colors
+BLACK="\e[0;30m"
+RED="\e[0;31m"
+GREEN="\e[0;32m"
+YELLOW="\e[0;33m"
+BLUE="\e[0;34m"
+MAGENTA="\e[0;35m"
+CYAN="\e[0;36m"
+GRAY="\e[0;37m"
+WHITE="\e[0;97m"
+CLEAR="\e[0m"
 
-	# if nothing is passed in reset the color
-	if [ -z "$1" ]; then color="none"; fi
-	
-	# starting block
-	printf "\033["
-	
-	# bold ? 
-	if [ "$3" == "bold" ]; then
-		printf "1;"
-	fi
-	
-	# the actual color code
-	printf "${!color}m"
-	
-	# some text passed in
-	if [ -n "$2" ]; then
-		printf "$2\033[m"
-	fi
-}
-export -f style
-
-# get git branch if this directory is a git repository
-# https://gist.github.com/clozed2u/4971506
-git_branch() {
-	# get git branch
-	local branch=$(git symbolic-ref --short HEAD 2>/dev/null)
-
-	if [ -n "$branch" ]; then		
-		local stat=$(git status 2>&1)
-		
-		if [[ "$stat" != *'working directory clean'* ]]; then
-			style red " [$branch]"
-		elif [[ "$stat" == *'Your branch is ahead'* ]]; then
-			style yellow " [$branch]"
-		elif [[ "$stat" == *'Your branch is behind'* ]]; then
-			style dark_grey " [$branch]"
-		else
-			style cyan " [$branch]"
-		fi
-	fi
-}
-
-bind "set completion-ignore-case on"
-bind "set show-all-if-ambiguous on"
-
-# some handy alias
-alias ls='ls -Glhp'
-alias h='history | grep'
-alias edit='sublime'
+# source in git completion and git prompt
+source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
+source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash
 
 if [[ $MACHTYPE =~ apple ]]; then
 	# OSX specific
@@ -80,15 +25,38 @@ else
 	export LSCOLORS='di=33:ln=36:fi=0:pi=36:ex=32:so=1;35:bd=01;33:cd=01;33:or=37:mi=37'
 fi
 
-# save user name for PS1
-USERNAME=$(finger $USER | head -1 | cut -d : -f 3)
-
 # Change the command line style
-export PS1='\n$(style green "#$USERNAME [\T] \w/"; git_branch)\n'
+# get the user display name rather than handle
+USERNAME=$(finger $USER | head -1 | cut -d : -f 3)
+PROMPT_COMMAND='__git_ps1 "\\n$CYAN# $USERNAME [\T] $YELLOW\w/$CLEAR" "\\n"'
 
+# some handy history control
 export HISTCONTROL="erasedups:ignoreboth"
-export HISTTIMEFORMAT="$(style yellow)%h %d %H:%M:%S > $(style none)"
-# export HISTIGNORE="ls*:cd*:echo*"
+export HISTTIMEFORMAT="$YELLOW%h %d %H:%M:%S > $CLEAR"
+export HISTIGNORE="ls*:cd*:echo*"
+alias h='history | grep'
+
+# use sublime to edit files
+if hash sublime 2>/dev/null; then
+	alias edit='sublime'
+fi
+
+# setup nvm if available
+if [ -d $HOME/.nvm ]; then
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+fi
+
+# ignore case for auto complete
+bind "set completion-ignore-case on"
+bind "set show-all-if-ambiguous on"
+
+# some handy alias
+alias ls='ls -Glhp'
+alias gp='git pull'
+alias gf='git fetch -p'
+alias gc='git checkout'
+alias gclean='git fetch -p && git branch | grep -Ev "master|dev" | xargs git branch -D'
 
 # export a variable so we dont re-setup the profile
 export _COMMON_PROFILE_SET_=1
