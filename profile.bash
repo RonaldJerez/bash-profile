387ec60119398ae3d7a065d1bec4daeb656cfed8
Ronald Jerez <RonaldJerez@users.noreply.github.com>
2016-12-01 16:34:11 -0500

    needed more control of the git promt, removed the sourced file and reverted back to own function

--- profile.bash
+++ profile.bash
@@ -13,8 +13,125 @@ GRAY="\e[0;37m"
 WHITE="\e[0;97m"
 CLEAR="\e[0m"
 
-# source in git completion and git prompt
-source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-prompt.sh
+# function to style console output
+# you can combine, color, style, or make color lighther
+# combining of format (bold/underline etc) is not supported
+#
+# Usage:
+# style red "some text"
+# style light red "some text"
+# style bold yellow "something else"
+# style underline light green "green text"
+style() {
+	local format=0
+	local light=0
+
+	while [ $# -gt 2 ]; do
+		case "$1" in
+			light)		light=1 ;;
+			bold)		format=1 ;;
+			underline)	format=4 ;;
+			blink)		format=5 ;;
+			invert)		format=7 ;;
+			hidden)		format=8 ;;
+		esac
+		shift
+	done
+
+	local color=$(echo "$1" | tr '[:lower:]' '[:upper:]') #convert color name to upper
+	local code="${!color}" # get the code of the color via expansion
+
+	(($light)) && code=${code/3/9}
+	(($format)) && code=${code/0/$format}
+
+	printf "$code" # print the style code
+	[ -n "$2" ] && printf "$2$CLEAR" # print text if available
+}
+
+# git status information based on
+# https://github.com/necolas/dotfiles/blob/master/shell/bash_prompt
+prompt_git() {
+	local s=""
+	local branchName=""
+	local remoteBranchName=""
+
+	local CLEAN_COLOR=${GIT_PROMPT_CLEAN_COLOR:-"green"}
+	local DIRTY_COLOR=${GIT_PROMPT_DIRTY_COLOR:-"red"}
+	local AHEAD_COLOR=${GIT_PROMPT_AHEAD_COLOR:-"light cyan"}
+	local BEHIND_COLOR=${GIT_PROMPT_BEHIND_COLOR:-"light black"}
+
+	# lets assume clean
+	local color=$CLEAN_COLOR
+
+	# check if the current directory is in a git repository
+	if [ $(git rev-parse --is-inside-work-tree &>/dev/null; printf "%s" $?) == 0 ]; then
+
+		# check if the current directory is in .git before running git checks
+		if [ "$(git rev-parse --is-inside-git-dir 2> /dev/null)" == "false" ]; then
+
+			# ensure index is up to date
+			git update-index --really-refresh -q &>/dev/null
+
+			if [[ -n $GIT_PROMPT_SHOW_DIRTY ]]; then
+				# check for uncommitted changes in the index
+				if ! $(git diff --quiet --ignore-submodules --cached); then
+					s="$s+"
+					color=$DIRTY_COLOR
+				fi
+
+				# check for unstaged changes
+				if ! $(git diff-files --quiet --ignore-submodules --); then
+					s="$s!"
+					color=$DIRTY_COLOR
+				fi
+
+				# check for untracked files
+				if [ -n "$(git ls-files --others --exclude-standard)" ]; then
+					s="$s?"
+					color=$DIRTY_COLOR
+				fi
+			fi
+
+			# check for stashed files
+			if [[ -n $GIT_PROMPT_SHOW_STASH && $(git rev-parse --verify refs/stash &>/dev/null) ]]; then
+				s="$s$"
+			fi
+
+			# # check weather we are ahead or behind remote branch
+			if [[ -n $GIT_PROMPT_SHOW_BEHIND || -n $GIT_PROMPT_SHOW_AHEAD ]]; then
+				remoteBranchName="$(git rev-parse --abbrev-ref --symbolic-full-name @{u} 2>/dev/null)"
+				remoteBranchName="${remoteBranchName/'@{u}'/}"
+
+				if [[ -n $remoteBranchName ]]; then
+					if [[ -n $GIT_PROMPT_SHOW_AHEAD && $(git rev-list --count --left-only HEAD...$remoteBranchName) > 0 ]]; then
+						color=$AHEAD_COLOR
+					fi
+
+					if [[ -n $GIT_PROMPT_SHOW_BEHIND && $(git rev-list --count --right-only HEAD...$remoteBranchName) > 0 ]]; then
+						color=$BEHIND_COLOR
+					fi
+				fi
+			fi
+
+		fi
+
+		# get the short symbolic ref
+		# if HEAD isn't a symbolic ref, get the short SHA
+		# otherwise, just give up
+		branchName="$(git symbolic-ref --quiet --short HEAD 2> /dev/null || \
+					  git rev-parse --short HEAD 2> /dev/null || \
+					  printf "(unknown)")"
+
+		[ -n "$s" ] && s="[$s]"
+
+		printf " on "
+		style $color "$branchName $s"
+	else
+		return
+	fi
+}
+
+# source in git completion
 source /Applications/Xcode.app/Contents/Developer/usr/share/git-core/git-completion.bash
 
 if [[ $MACHTYPE =~ apple ]]; then
@@ -28,11 +145,12 @@ fi
 # Change the command line style
 # get the user display name rather than handle
 USERNAME=$(finger $USER | head -1 | cut -d : -f 3)
-PROMPT_COMMAND='__git_ps1 "\\n$CYAN#$USERNAME [\T] $YELLOW\w$CLEAR" "\\n" " | %s "'
+#PROMPT_COMMAND='__git_ps1 "\\n$CYAN#$USERNAME [\T] $YELLOW\w$CLEAR" "\\n" " | %s "'
+export PS1='\n$(style cyan "#$USERNAME [\T] "; style yellow "\w"; prompt_git)\n'
 
 # some handy history control
 export HISTCONTROL="erasedups:ignoreboth"
-export HISTTIMEFORMAT="$YELLOW%h %d %H:%M:%S > $CLEAR"
+export HISTTIMEFORMAT="$(style green)%h %d %H:%M:%S > $(style clear)"
 export HISTIGNORE="ls*:cd*:echo*"
 alias h='history | grep'
 
